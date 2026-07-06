13af2849cbc7df65fad261d77d863b30c34f1bb6
omareldesoky00 <omar.eldesoky02@gmail.com>
2026-07-06 21:37:52 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Using a relative base ('./') so the build works when hosted at
-// https://<username>.github.io/<repo-name>/ without any extra config.
 export default defineConfig({
   plugins: [react()],
-  base: './',
-})
+  base: "/newDashBoard/",
+});
