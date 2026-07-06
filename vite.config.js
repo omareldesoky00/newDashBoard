7db25f2ad7e70b42a1854737b434b5ece620796e
omareldesoky00 <omar.eldesoky02@gmail.com>
2026-07-06 17:38:04 +0300

    Trigger Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // Using a relative base ('./') so the build works when hosted at
 // https://<username>.github.io/<repo-name>/ without any extra config.
